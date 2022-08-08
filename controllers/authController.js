4f32e09778a74e5bf3836d974c3a59cc6b0d0ab8
Ozan <ozan.yasin97@gmail.com>
2022-08-08 09:01:45 +0300

    Front-end and Server Connection done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,32 @@
 import User from '../models/User.js';
 import { StatusCodes } from 'http-status-codes';
+import { BadRequestError } from '../errors/index.js';
 
 const register = async (req, res) => {
   // Do not need to try catch block due to express-async-errors library
-  const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError('please provide all values');
+  }
+
+  const userAlreadyExist = await User.findOne({ email });
+  if (userAlreadyExist) {
+    throw new BadRequestError('Email already in use.');
+  }
+  const user = await User.create({ name, email, password });
+  const token = user.createJWT();
+  res
+    .status(StatusCodes.CREATED)
+    .json({
+      user: {
+        email: user.email,
+        lastName: user.lastName,
+        location: user.location,
+        name: user.name,
+      },
+      token,
+    });
 };
 
 const login = async (req, res) => {
